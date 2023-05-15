00be4f9a1a470a074f3ba5532d0f5df9fbc39872
javiersanchz <javiersanchzgil@gmail.com>
2023-05-15 19:32:09 +0200

    Pantalla principal

---
+++ app/src/main/java/com/example/tfg/LocalesDesayuno.kt
@@ -0,0 +1,119 @@
+package com.example.tfg
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
+
+class LocalesDesayuno : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: RestauranteAdapter
+    private lateinit var restaurantes: ArrayList<Locales>
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_locales_desayuno)
+
+            listView = findViewById(R.id.listView2)
+
+            restaurantes = arrayListOf(
+                Locales("Choose Restaurante Vegano", "Restaurante que sirve versiones veganas de platos típicos italianos, incluidas pastas, pizzas y risottos", R.drawable.chooserestaurante, "Choose Restaurante Vegano"),
+                Locales("Punto Vegano", "Restaurante de comida vegetariana estricta con variedad de platos", R.drawable.puntovegano, "Punto Vegano"),
+                Locales("Distrito Vegano Invernadero", "Restaurante de comida vegetariana estricta con variedad de platos", R.drawable.distritovegano, "Distrito Vegano Invernadero"),
+                Locales("Mudrá Plant Based Food - Restaurante Vegano", "Restaurante refinado y moderno con un menú de platos veganos creativos.", R.drawable.mudraplant, "Mudrá Plant Based Food - Restaurante Vegano "),
+                Locales("VEGA", "Restaurante chic de cocina vegana con platos veganos, postres y bebidas naturales", R.drawable.vega, "VEGA")
+            )
+
+            adapter = RestauranteAdapter(this, restaurantes)
+            listView.adapter = adapter
+
+            listView.setOnItemClickListener { _, _, position, _ ->
+                val restaurante = restaurantes[position]
+                val location = Uri.encode(restaurante.mapa)
+                val uri = Uri.parse("geo:0,0?q=$location")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                intent.setPackage("com.google.android.apps.maps")
+                startActivity(intent)
+            }
+        }
+
+        private inner class RestauranteAdapter(
+            private val context: Context,
+            private val restaurantes: List<Locales>
+        ) : BaseAdapter() {
+
+            override fun getCount(): Int {
+                return restaurantes.size
+            }
+
+            override fun getItem(position: Int): Any {
+                return restaurantes[position]
+            }
+
+            override fun getItemId(position: Int): Long {
+                return position.toLong()
+            }
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                val view: View
+                val viewHolder: ViewHolder
+
+                if (convertView == null) {
+                    view = LayoutInflater.from(context).inflate(R.layout.list_item_restaurante, null)
+                    viewHolder = ViewHolder()
+                    viewHolder.nombreTextView = view.findViewById(R.id.nombreTextView)
+                    viewHolder.descripcionTextView = view.findViewById(R.id.descripcionTextView)
+                    viewHolder.buscarTextView = view.findViewById(R.id.buscarTextView)
+                    viewHolder.fotoImageView = view.findViewById(R.id.fotoImageView)
+                    view.tag = viewHolder
+                } else {
+                    view = convertView
+                    viewHolder = convertView.tag as ViewHolder
+                }
+
+                val restaurante = restaurantes[position]
+
+                viewHolder.nombreTextView.text = restaurante.nombre
+                viewHolder.descripcionTextView.text = restaurante.descripcion
+                viewHolder.buscarTextView.setOnClickListener {
+                    val location = Uri.encode(restaurante.mapa)
+                    val uri = Uri.parse("geo:0,0?q=$location")
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    intent.setPackage("com.google.android.apps.maps")
+                    startActivity(intent)
+                }
+
+                viewHolder.fotoImageView.setImageResource(restaurante.imagen)
+
+                return view
+            }
+
+
+        }
+
+        private inner class ViewHolder {
+            lateinit var nombreTextView: TextView
+            lateinit var descripcionTextView: TextView
+            lateinit var buscarTextView: TextView
+            lateinit var fotoImageView: ImageView
+        }
+
+        private data class Locales(
+            val nombre: String,
+            val descripcion: String,
+            val imagen: Int,
+            val mapa: String
+        )
+    }
+
--- app/src/main/java/com/example/tfg/pantallaPrincipal.kt
+++ app/src/main/java/com/example/tfg/pantallaPrincipal.kt
@@ -3,23 +3,78 @@ package com.example.tfg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 import com.example.tfg.databinding.ActivityPantallaPrincipalBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Calendar
 
 class pantallaPrincipal : AppCompatActivity() {
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var textoAnimacion: String
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_principal)
+
+
 
         //binding
         val binding = ActivityPantallaPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        val userEmail = auth.currentUser?.email
+        val documentReference = userEmail?.let { firestore.collection("usuarios").document(it) }
+        if (documentReference != null) {
+            documentReference.get().addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val nombre = documentSnapshot.getString("nombre")
+                    val saludo = obtenerSaludo()
+                    val mensaje = "$saludo $nombre"
+                    textView = findViewById<TextView>(R.id.textSaludo)
+                    textoAnimacion = mensaje
+                    // Aquí puedes mostrar el mensaje en tu pantalla principal
+
+                    animarTexto()
+                }
+            }.addOnFailureListener { exception ->
+                // Maneja el error si la lectura de datos falla
+            }
+        }
 
         //que el boton de restaurantes te lleve a la pantalla de restaurantes
         binding.boton4.setOnClickListener {
-            val intent = Intent(this, Restaurantes::class.java)
+            val intent = Intent(this, BuscarLocales::class.java)
             startActivity(intent)
         }
     }
+
+    private fun obtenerSaludo(): String {
+        val calendar = Calendar.getInstance()
+        val hora = calendar.get(Calendar.HOUR_OF_DAY)
+        return when (hora) {
+            in 6 until 12 -> "Buenos días"
+            in 12 until 20 -> "Buenas tardes"
+            else -> "Buenas noches"
+        }
+    }
+    private fun animarTexto() {
+        val handler = Handler(Looper.getMainLooper())
+
+        val builder = StringBuilder()
+
+        for (i in textoAnimacion.indices) {
+            handler.postDelayed({
+                builder.append(textoAnimacion[i])
+                textView.text = builder.toString()
+            }, i * 150L)
+        }
+    }
+
 }
